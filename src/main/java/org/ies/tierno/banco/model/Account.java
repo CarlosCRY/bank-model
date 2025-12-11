78307c9d6b447338e8fea266be484bc25639ce3a
CarlosCRY <trueccr@gmail.com>
2025-12-11 08:41:46 +0100

    Cambiar a formato con lombok

--- src/main/java/org/ies/tierno/banco/model/Account.java
+++ src/main/java/org/ies/tierno/banco/model/Account.java
@@ -1,16 +1,15 @@
 package org.ies.tierno.banco.model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 
 public class Account {
     private String iban;
     private double balance;
     private Client client;
-
-    public Account (String iban, double balance, Client client) {
-        this.iban = iban;
-        this.balance = balance;
-        this.client = client;
     }
 
     public void showInfo() {
@@ -27,50 +26,5 @@ public class Account {
         } else {
             balance -= amount;
         }
-    }
-
-    public String getIban () {
-        return iban;
-    }
-
-    public void setIban (String iban) {
-        this.iban = iban;
-    }
-
-    public double getBalance () {
-        return balance;
-    }
-
-    public void setBalance (double balance) {
-        this.balance = balance;
-    }
-
-    public Client getClient () {
-        return client;
-    }
-
-    public void setClient (Client client) {
-        this.client = client;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return Double.compare(balance, account.balance) == 0 && Objects.equals(iban, account.iban) && Objects.equals(client, account.client);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(iban, balance, client);
-    }
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "iban='" + iban + '\'' +
-                ", balance=" + balance +
-                ", client=" + client +
-                '}';
     }
 }
